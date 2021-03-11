19973a1e12d8511082f5b3a9be715e4164ce301d
Carlos Gabriel da Silva Machado <cgdsm.eng18@uea.edu.br>
2021-03-10 20:54:42 -0400

    Exercising my python skills

--- URI/uri_1018.py
+++ URI/uri_1018.py
@@ -1,3 +1,18 @@
 # -*- coding: utf-8 -*-
 
 valor = int(input())
+
+mensagem = "{}\n{} nota(s) de R$ 100,00\n{} nota(s) de R$ 50,00\n" +\
+    "{} nota(s) de R$ 20,00\n{} nota(s) de R$ 10,00\n" +\
+    "{} nota(s) de R$ 5,00\n{} nota(s) de R$ 2,00\n{} nota(s) de R$ 1,00"
+
+cem = valor // 100
+cinquenta = (valor % 100) // 50
+vinte = ((valor % 100) % 50) // 20
+dez = (((valor % 100) % 50) % 20) // 10
+cinco = ((((valor % 100) % 50) % 20) % 10) // 5
+dois = (((((valor % 100) % 50) % 20) % 10) % 5) // 2
+um = ((((((valor % 100) % 50) % 20) % 10) % 5) % 2) // 1
+
+
+print(mensagem.format(valor, cem, cinquenta, vinte, dez, cinco, dois, um))
